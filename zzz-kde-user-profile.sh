6dba5a612e82dbaa99aee25caa9210f36c7cb9ab
Frank Schütte <fschuett@gymhim.de>
2020-07-21 12:19:48 +0200

    fix config/data dir collection

--- zzz-kde-user-profile.sh
+++ zzz-kde-user-profile.sh
@@ -5,11 +5,12 @@ BASEDIR=/var/lib/kde-profiles/
 
 if [ -e /etc/kde-user-profile ]; then
   TEACHERDIRS="$(cat /etc/kde-user-profile|grep '^teachers='|awk -F\= '{ print $2 }'| tr ',' ' ')"
+  STUDENTDIRS="$(cat /etc/kde-user-profile|grep '^students='|awk -F\= '{ print $2 }'| tr ',' ' ')"
   SCHUELERDIRS="$(cat /etc/kde-user-profile|grep '^schueler='|awk -F\= '{ print $2 }'| tr ',' ' ')"
   KLASSENARBEITDIRS="$(cat /etc/kde-user-profile|grep '^klassenarbeit='|awk -F\= '{ print $2 }'| tr ',' ' ')"
 fi
 
-if [ -n "${TEACHERDIRS}${SCHUELERDIRS}${KLASSENARBEITDIRS}" ]; then
+if [ -n "${TEACHERDIRS}${STUDENTDIRS}${SCHUELERDIRS}${KLASSENARBEITDIRS}" ]; then
   case $USER in
     schueler)
       for dir in $SCHUELERDIRS; do
@@ -24,21 +25,24 @@ if [ -n "${TEACHERDIRS}${SCHUELERDIRS}${KLASSENARBEITDIRS}" ]; then
       done;
     ;;
     *)
-      for dir in default; do
-        test -d "$BASEDIR$dir/share" && XDG_DATA_DIRS="$BASEDIR$dir/share:$XDG_DATA_DIRS"
-        test -d "$BASEDIR$dir/config" && XDG_CONFIG_DIRS="$BASEDIR$dir/config:$XDG_CONFIG_DIRS"
-      done;
     ;;
   esac
   
-  if id -gn $USER|grep -qw teachers; then
+  if id -Gn $USER|grep -qiw teachers; then
       for dir in $TEACHERDIRS; do
         test -d "$BASEDIR$dir/share" && XDG_DATA_DIRS="$BASEDIR$dir/share:$XDG_DATA_DIRS"
         test -d "$BASEDIR$dir/config" && XDG_CONFIG_DIRS="$BASEDIR$dir/config:$XDG_CONFIG_DIRS"
       done;
   fi
   
-  unset TEACHERDIRS SCHUELERDIRS KLASSENARBEITDIRS
+  if id -Gn $USER|grep -qiw students; then
+      for dir in $STUDENTDIRS; do
+        test -d "$BASEDIR$dir/share" && XDG_DATA_DIRS="$BASEDIR$dir/share:$XDG_DATA_DIRS"
+        test -d "$BASEDIR$dir/config" && XDG_CONFIG_DIRS="$BASEDIR$dir/config:$XDG_CONFIG_DIRS"
+      done;
+  fi
+  
+  unset TEACHERDIRS STUDENTDIRS SCHUELERDIRS KLASSENARBEITDIRS
   
   export XDG_DATA_DIRS XDG_CONFIG_DIRS
   
